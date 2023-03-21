2b4028ac56b30c95e489e92c3ff353b2ef9678d4
Boris Batanov <majorgucsky@gmail.com>
2023-03-21 21:58:40 +0300

    Update

--- js/index.js
+++ js/index.js
@@ -42,10 +42,3 @@ modalCloseBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
   modal.classList.remove('is-open');
 });
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
-    modal.classList.remove('is-open');
-  }
-});
